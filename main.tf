9cf8d1ab99e8ec335a99ff28d2bc6a3f86b8394d
Zone <myz.defi@gmail.com>
2024-07-31 14:59:19 +0800

    update

--- main.tf
+++ main.tf
@@ -21,10 +21,10 @@ resource "aws_s3_bucket_website_configuration" "memoo" {
 
   routing_rule {
     condition {
-      # key_prefix_equals = "docs/"
+      key_prefix_equals = "docs/"
     }
     redirect {
-      # replace_key_prefix_with = "documents/"
+      replace_key_prefix_with = "documents/"
     }
   }
 }
